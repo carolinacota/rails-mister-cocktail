a55103093d29a1a9cacfb30671bc7e5b0e9cd81e
Carolina Cota <carolinareycota@gmail.com>
2020-02-23 18:56:45 +0100

    Updating seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Destroy ingredients"
+Ingredient.destroy_all
+
+puts "Destroy Cocktails"
+Cocktail.destroy_all
+
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "pepper")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "cachaça")
+Ingredient.create(name: "sugar")
